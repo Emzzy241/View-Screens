22e5a7abfd2d1ac29934e41e91dde667a13a199b
Emzzy241 <emzzyoluwole@gmail.com>
2024-05-15 04:42:17 +0100

    add all

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+/**
+ * add event on element
+ */
+
+const addEventOnElem = function (elem, type, callback) {
+    if (elem.length > 1) {
+      for (let i = 0; i < elem.length; i++) {
+        elem[i].addEventListener(type, callback);
+      }
+    } else {
+      elem.addEventListener(type, callback);
+    }
+  }
+
+
+// Accordion Toggle
+
+const accordionAction = document.querySelectorAll("[data-accordion-action]");
+
+const toggleAccordion = function () { this.classList.toggle("active"); }
+
+addEventOnElem(accordionAction, "click", toggleAccordion);
